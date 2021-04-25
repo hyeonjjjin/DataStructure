f281aefb82a19e8ba25b3b8abb0f7c854c7e650c
hjjja <jahj30734@gmail.com>
2021-04-25 23:29:02 +0900

    lecture4_start

--- src/DataStructure_1.java
+++ src/DataStructure_1.java
@@ -1,9 +1,8 @@
 public class DataStructure_1 {
-    public static void lec1_5(){
+    public static void Lec4_Array(){
 
     }
     public static void main(String[] args) {
         System.out.println("생활코딩 님의 자료구조 강의 수강 기록!");
-
     }
 }
